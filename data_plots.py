4f376367db9365ca5a6a529e51b471bc5646742b
Alli Wong <wonga10@gmail.com>
2018-06-01 15:05:59 -0700

    Add code for plots

--- data_plots.py
+++ data_plots.py
@@ -6,10 +6,22 @@ import seaborn as sns; sns.set(style="ticks", color_codes=True)
 # Read dataset
 df = pd.read_csv('sim_stats.csv')
 
-sns.lmplot(x='instantiation times',
-           y='response times',
-           data=df,
-           hue='scheduler',
-           col='plist configuration')
+g = sns.factorplot(x='plist configuration',
+                   y='average queue lengths',
+                   hue='scheduler',
+                   data = df,
+                   size = 8,
+                   kind = "bar",
+                   palette = "muted"
+                   )
 plt.show()
 print("hello")
+g = sns.factorplot(x='plist configuration',
+                   y='total wait times',
+                   hue='scheduler',
+                   data = df,
+                   size = 8,
+                   kind = "bar",
+                   palette = "muted"
+                   )
+plt.show()
