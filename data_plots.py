f44fb47b99fa7925674c8ada42382ee07571d14d
natreed <natreed@pdx.edu>
2018-06-11 10:22:43 -0700

    more plots

--- data_plots.py
+++ data_plots.py
@@ -8,13 +8,35 @@ import seaborn as sns; sns.set(style="ticks", color_codes=True)
 df = pd.read_csv('sim_stats.csv')
 
 g = sns.factorplot(x='scheduler',
+                   y='efficiency',
+                   data=df,
+                   size=8,
+                   kind="bar",
+                   #palette= sns.xkcd_palette(colors)
+                   )
+plt.savefig('Plots/scheduler_x_efficiency_y_500_100k')
+exit(0)
+g = sns.factorplot(x='scheduler',
                    y='average wait times',
                    data=df,
                    size=8,
                    kind='bar',
                    #palette= sns.xkcd_palette(colors)
                    )
-plt.savefig('Plots/avg_wait')
+plt.savefig('Plots/avg_wait_500_100k')
+
+g = sns.lmplot(x='instantiation times',
+                   y='turnaround stat',
+                   data=df,
+                   hue='scheduler',
+                   col='process sizes',
+                   sharex=False,
+                   sharey=False,
+                   #palette= sns.xkcd_palette(colors)
+                   )
+plt.savefig('Plots/instantiation_x_turnaround_stat_y_500_100k')
+
+
 
 
 g = sns.factorplot(x='plist configuration',
@@ -25,7 +47,7 @@ g = sns.factorplot(x='plist configuration',
                    kind="bar",
                    #palette= sns.xkcd_palette(colors)
                    )
-plt.savefig('Plots/config_x_avg_queue_lens_y')
+plt.savefig('Plots/config_x_avg_queue_lens_y_500_100k')
 
 g = sns.lmplot(x='required cpu time',
                    y='turnaround times',
@@ -38,7 +60,7 @@ g = sns.lmplot(x='required cpu time',
                    )
 g.despine(left=True)
 plt.legend(loc='upper right')
-plt.savefig('Plots/cpu_time_x_turnaround_time_y')
+plt.savefig('Plots/cpu_time_x_turnaround_time_y_500_100k')
 
 
 g = sns.factorplot(x='scheduler',
@@ -48,16 +70,9 @@ g = sns.factorplot(x='scheduler',
                    kind="bar",
                    #palette= sns.xkcd_palette(colors)
                    )
-plt.savefig('Plots/average_response_time_hist')
+plt.savefig('Plots/average_response_time_hist_500_100k')
 
-g = sns.factorplot(x='scheduler',
-                   y='efficiency',
-                   data=df,
-                   size=8,
-                   kind="bar",
-                   #palette= sns.xkcd_palette(colors)
-                   )
-plt.savefig('Plots/scheduler_x_efficiency_y')
+
 
 g = sns.factorplot(x='scheduler',
                    y='average queue length',
@@ -66,7 +81,7 @@ g = sns.factorplot(x='scheduler',
                    kind="bar",
                    #palette= sns.xkcd_palette(colors)
                    )
-plt.savefig('Plots/avg_queue_lengths')
+plt.savefig('Plots/avg_queue_lengths_500_100k')
 
 g = sns.lmplot(x='instantiation times',
                    y='turnaround stat',
@@ -77,7 +92,7 @@ g = sns.lmplot(x='instantiation times',
                    sharey=False,
                    #palette= sns.xkcd_palette(colors)
                    )
-plt.savefig('Plots/instantiation_times_x_turnaround_stat_y')
+plt.savefig('Plots/instantiation_times_x_turnaround_stat_y_500_100k')
 
 g = sns.lmplot(x='start_times',
                    y='finish times',
@@ -91,7 +106,7 @@ g = sns.lmplot(x='start_times',
                    )
 g.despine(left=True)
 plt.legend(loc='upper right')
-plt.savefig('Plots/start_times_v_finish_times')
+plt.savefig('Plots/start_times_v_finish_times_500_100k')
 
 
 
